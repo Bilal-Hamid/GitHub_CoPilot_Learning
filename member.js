fd3b04e503d0f3bf2a202047ed5c8fe1fffb8461
Bilal Hamid <124129770+Bilal-Hamid@users.noreply.github.com>
2024-12-08 08:43:49 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember(){
+    console.log('I am a member');
+    console.log('I have the following skills:');
+    console.log('1. HTML');
+    console.log('2. CSS');
+    console.log('3. Javascript');
+    console.log('4. Node.js');
+}
